b8ce00603fcc9a3fa97c755fea886ebe5eb03ce7
Terin Stock <terinjokes@gmail.com>
2015-09-23 14:24:28 -0700

    refactor(base64): return Observable for base64.encode

--- tests/base64.js
+++ tests/base64.js
@@ -2,33 +2,51 @@
 var expect = require('assume');
 var base64 = require('../lib/utils/base64');
 var fs = require('mz/fs');
+var Rx = require('rx');
 var path = require('path');
 
 describe('base64 utility', function() {
   describe('encode', function() {
-    it('should base64 encode ASCII text', function() {
-      return base64.encode(new Buffer('plain text')).then(function(encoded) {
-        expect(encoded).is.a('string');
-        expect(encoded).matches(/^data:text\/plain; charset=us-ascii;base64,/);
-      });
+    it('should base64 encode ASCII text', function(done) {
+      base64.encode(new Buffer('plain text'))
+        .subscribe(function(encoded) {
+          try {
+            expect(encoded).is.a('string');
+            expect(encoded).matches(/^data:text\/plain; charset=us-ascii;base64,/);
+            done();
+          } catch(e) {
+            done(e);
+          }
+        }, done);
     });
 
-    it('should base64 encode Unicode text', function() {
+    it('should base64 encode Unicode text', function(done) {
       var buf = new Buffer([0xF0, 0x9F, 0x9A, 0x97]);
 
-      return base64.encode(buf).then(function(encoded) {
-        expect(encoded).is.a('string');
-        expect(encoded).matches(/^data:text\/plain; charset=utf-8;base64,/);
-      });
+      base64.encode(buf)
+        .subscribe(function(encoded) {
+          try {
+            expect(encoded).is.a('string');
+            expect(encoded).matches(/^data:text\/plain; charset=utf-8;base64,/);
+            done();
+          } catch(e) {
+            done(e);
+          }
+        }, done);
     });
 
-    it('should base64 encode a GIF', function() {
-      return fs.readFile(path.join(__dirname, 'fixtures/gif.gif'))
-        .then(base64.encode)
-        .then(function(encoded) {
-          expect(encoded).is.a('string');
-          expect(encoded).matches(/^data:image\/gif; charset=binary;base64,/);
-        });
+    it('should base64 encode a GIF', function(done) {
+      Rx.Observable.fromPromise(fs.readFile(path.join(__dirname, 'fixtures/gif.gif')))
+        .flatMap(base64.encode)
+        .subscribe(function(encoded) {
+          try {
+            expect(encoded).is.a('string');
+            expect(encoded).matches(/^data:image\/gif; charset=binary;base64,/);
+            done();
+          } catch(e) {
+            done(e);
+          }
+        }, done);
     });
   });
 
